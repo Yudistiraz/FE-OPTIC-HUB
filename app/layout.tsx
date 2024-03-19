1ebc8e1593445463069868a1d70a8c788d1485ee
Yudistiraz <yudistiramastra@gmail.com>
2024-03-19 21:56:06 +0700

    implement PWA

--- app/layout.tsx
+++ app/layout.tsx
@@ -11,11 +11,15 @@ export const metadata: Metadata = {
 
 export default function RootLayout({
   children,
-}: Readonly<{
+}: {
   children: React.ReactNode;
-}>) {
+}) {
   return (
     <html lang="en">
+      <head>
+        <link rel="manifest" href="/manifest.json" />
+        <meta name="theme-color" content="#000000" />
+      </head>
       <body className={inter.className}>{children}</body>
     </html>
   );
