395f9f3510b6a52351aa8e299a50e74e723466f0
Yudistiraz <yudistiramastra@gmail.com>
2024-03-21 16:08:21 +0700

    fix : moved general page

---
+++ app/(general)/default-layout.tsx
@@ -0,0 +1,45 @@
+"use client";
+
+import CustomContainer from "@/components/ui/Container";
+import CustomDrawer from "@/components/ui/Drawer";
+import Header from "@/components/ui/Header";
+import SidebarMenu from "@/components/ui/SidebarMenu";
+import { getSession } from "next-auth/react";
+import { useRouter } from "next/navigation";
+import { useEffect } from "react";
+
+export default function DefaultLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  {
+    children;
+  }
+  const router = useRouter();
+  const checkSession = async () => {
+    const session = await getSession();
+    if (!session) {
+      router.push("/signin");
+    }
+  };
+
+  useEffect(() => {
+    checkSession();
+  }, []);
+
+  return (
+    <div className="tw-flex">
+      <CustomDrawer
+        variant="permanent"
+        drawerPaperClass="tw-bg-primary-500 tw-w-[270px]"
+        containerClass="tw-px-4"
+      >
+        <SidebarMenu />
+      </CustomDrawer>
+
+      {/* <Header /> */}
+      <CustomContainer className="">{children}</CustomContainer>
+    </div>
+  );
+}
--- app/(general)/page.tsx
+++ app/(general)/page.tsx
@@ -1,12 +1,8 @@
 "use client";
 
-import { signOut, useSession } from "next-auth/react";
+import { signOut } from "next-auth/react";
 
 export default function Home() {
-  const { data: session, status } = useSession();
-  console.log(session);
-  console.log(status);
-
   return (
     <div className="">
       <button
