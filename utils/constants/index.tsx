53cc029e19df33774730ad1f311e8442f349a5f1
Yudistiraz <yudistiramastra@gmail.com>
2024-03-21 16:07:47 +0700

    feat : add constant

---
+++ utils/constants/index.tsx
@@ -0,0 +1,182 @@
+export const SITEMAPS = [
+  {
+    name: "Home",
+    path: "/",
+  },
+  {
+    name: "My Account",
+    path: "/profile",
+  },
+];
+
+export const MOBILE_SITEMAPS = [
+  {
+    name: "Cart",
+    path: "/shopping-cart",
+  },
+  {
+    name: "Notification",
+    path: "/notification",
+  },
+];
+
+export const STAFF_SITEMAPS = [
+  {
+    name: "Dashboard",
+    path: "/",
+    img: "dashboard",
+  },
+  {
+    name: "Schedule",
+    path: "/schedule",
+    img: "schedule",
+  },
+  {
+    name: "Sales",
+    path: "/sales",
+    img: "sales",
+  },
+  {
+    name: "Promotion Banner",
+    path: "/promotion-banner",
+    img: "promotionBanner",
+  },
+  {
+    name: "Customer",
+    path: "/customer",
+    img: "customer",
+  },
+];
+
+export const OWNER_SITEMAPS = [
+  {
+    name: "Dashboard",
+    path: "/",
+    img: "dashboard",
+  },
+  {
+    name: "Schedule",
+    path: "/schedule",
+    img: "schedule",
+  },
+  {
+    name: "Sales",
+    path: "/sales",
+    img: "sales",
+  },
+  {
+    name: "Promotion Banner",
+    path: "/promotion-banner",
+    img: "promotionBanner",
+  },
+  {
+    name: "Customer",
+    path: "/customer",
+    img: "customer",
+  },
+  {
+    name: "MANAGEMENT",
+    path: "",
+  },
+  {
+    name: "Classes",
+    path: "/classes",
+    img: "classes",
+  },
+  {
+    name: "Location",
+    path: "/location",
+    img: "location",
+  },
+  {
+    name: "Plan Package",
+    path: "/plan-package",
+    img: "planPackage",
+  },
+  {
+    name: "Instructor",
+    path: "/instructor",
+    img: "instructor",
+  },
+  {
+    name: "Class Layout",
+    path: "/class-layout",
+    img: "classLayout",
+  },
+  {
+    name: "Admin",
+    path: "/admin",
+    img: "admin",
+  },
+];
+
+export const DUMMY_SELECT = [
+  {
+    id: "1",
+    label: "Option 1",
+    value: "Option 1",
+  },
+  {
+    id: "2",
+    label: "Option 2",
+    value: "Option 2",
+  },
+];
+
+export const DUMMY_SELECT_OPTIONS = [
+  {
+    id: "1",
+    name: "Option 1",
+    label: "Option 1",
+  },
+  {
+    id: "2",
+    name: "Option 2",
+    label: "Option 2",
+  },
+];
+
+export const STATUS_OPTIONS = [
+  {
+    label: "Active",
+    value: "true",
+  },
+  {
+    label: "Inactive",
+    value: "false",
+  },
+];
+
+export const DAY_HOURS_OPTIONS = [
+  { label: "Days", value: "days" },
+  { label: "Hours", value: "hours" },
+];
+
+export const DAILY_WEEKLY_OPTIONS = [
+  { label: "Daily", value: "days" },
+  { label: "Weekly", value: "weeks" },
+];
+
+export const SCHEDULE_STATUS_OPTIONS = [
+  { label: "Upcoming", value: "upcoming" },
+  { label: "Finished", value: "finished" },
+  { label: "Cancelled", value: "cancelled" },
+];
+
+export const PHONE_PREFIX = [
+  { label: "Indonesia", value: "+62" },
+  { label: "Brunei Darussalam", value: "+673" },
+  { label: "Cambodia", value: "+855" },
+  { label: "Laos", value: "+856" },
+  { label: "Malaysia", value: "+60" },
+  { label: "Myanmar (Burma)", value: "+95" },
+  { label: "Philippines", value: "+63" },
+  { label: "Singapore", value: "+65" },
+  { label: "Thailand", value: "+66" },
+  { label: "Vietnam", value: "+84" },
+];
+
+export const STATUS_CLASSES_OPTIONS = [
+  { label: "Active", value: "active" },
+  { label: "Inactive", value: "inactive" },
+];
