b130e5f8da467bbb9363af1897cb0a60f683fb54
Yudistiraz <yudistiramastra@gmail.com>
2024-03-26 21:48:00 +0700

    feat : created custom switch component

---
+++ components/ui/Switch/index.tsx
@@ -0,0 +1,67 @@
+import { FormHelperText, Switch, Typography } from "@mui/material";
+import { useState } from "react";
+
+interface CustomSwitchProps {
+  label: string;
+  name: string;
+  disabled?: boolean;
+  className?: string;
+  activeText?: string;
+  inactiveText?: string;
+  helperText?: string;
+  onChange: (value: boolean) => void; // Fix the function type
+}
+
+function CustomSwitch({
+  label,
+  name,
+  onChange,
+  disabled = false,
+  activeText = "Active",
+  inactiveText = "Inactive",
+  className = "",
+  helperText = "",
+  ...rest
+}: CustomSwitchProps) {
+  const [checked, SetChecked] = useState(false);
+
+  return (
+    <div className="tw-w-full">
+      {label && (
+        <div>
+          <Typography
+            variant="labelSmall"
+            className="!tw-text-gray-600 tw-uppercase"
+          >
+            {label}
+          </Typography>
+        </div>
+      )}
+
+      <div className="tw-flex tw-items-center tw-gap-4">
+        <Switch
+          className={className}
+          name={name}
+          disabled={disabled}
+          onChange={() => {
+            SetChecked(!checked);
+            onChange(!checked);
+          }}
+          checked={checked}
+          {...rest}
+        />
+        <Typography variant="labelSmall">
+          {checked ? activeText : inactiveText}
+        </Typography>
+      </div>
+
+      {helperText.length > 0 && (
+        <FormHelperText error className="tw-italic tw-mx-0">
+          {helperText}
+        </FormHelperText>
+      )}
+    </div>
+  );
+}
+
+export default CustomSwitch;
