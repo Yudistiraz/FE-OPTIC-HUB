5390f6d651dd00e1444cfefcc949dd7fa52035c4
Yudistiraz <yudistiramastra@gmail.com>
2024-03-26 14:28:45 +0700

    feat : created custom badge component

---
+++ components/ui/CustomBadge/index.tsx
@@ -0,0 +1,39 @@
+import { Typography } from "@mui/material";
+import React, { Fragment } from "react";
+
+interface CustomBadgeProps {
+  status?: boolean;
+  trueLabel?: string;
+  falseLabel?: string;
+  trueColor?: string;
+  falseColor?: string;
+  textClasses?: string;
+}
+
+export default function CustomBadge({
+  status = true,
+  trueLabel = "",
+  falseLabel = "",
+  trueColor = "tw-bg-primary-500",
+  falseColor = "tw-bg-gray-700",
+  textClasses = "",
+}: CustomBadgeProps) {
+  return (
+    <Fragment>
+      <div
+        className={`tw-rounded tw-h-6 tw-w-24 tw-flex tw-items-center tw-justify-center ${
+          status ? trueColor : falseColor
+        }`}
+      >
+        <Typography
+          variant="customBadgeText"
+          className={`${textClasses} tw-text-white`}
+        >
+          {status ? trueLabel || "Active" : falseLabel || "Inactive"}
+        </Typography>
+      </div>
+    </Fragment>
+  );
+}
+
+//tw-w-20 tw-h-fit tw-flex tw-items-center tw-justify-center tw-p-1
