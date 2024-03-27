734b06930c496efbbb5e84bd73bc08e71cee5d8c
Yudistiraz <yudistiramastra@gmail.com>
2024-03-27 14:13:36 +0700

    feat : add function to check date and find data by id

--- utils/function/index.ts
+++ utils/function/index.ts
@@ -1,4 +1,16 @@
+import dayjs, { Dayjs } from "dayjs";
 export function gethelperText(isError: boolean, message: string) {
   if (isError) return message;
   else return "";
+}
+
+export function checkDateValidity(d: Dayjs) {
+  return dayjs(d).isValid();
+}
+
+export function findDataById<T extends { id: string }>(
+  dataArray: T[],
+  id: string
+): T | undefined {
+  return dataArray.find((item) => item.id === id);
 }
