d23b3419870a074829a4297d35d894525af41c09
Yudistiraz <yudistiramastra@gmail.com>
2024-03-27 14:12:59 +0700

    feat : add admin model

---
+++ utils/models/index.ts
@@ -0,0 +1,10 @@
+export type TEmployee = {
+  id: string;
+  dob: Date | string;
+  name: string;
+  email: string;
+  password: string;
+  phoneNumber: string;
+  status: boolean;
+  role: string;
+};
