6daa89cc6f21d8f673d62c429195ce748f2d0cd8
Yudistiraz <yudistiramastra@gmail.com>
2024-04-08 23:37:21 +0700

    feat : created dateformatter function

---
+++ utils/dateFormatter/index.ts
@@ -0,0 +1,20 @@
+import dayjs, { Dayjs } from "dayjs";
+import timezone from "dayjs/plugin/timezone";
+import utc from "dayjs/plugin/utc";
+
+dayjs.extend(utc);
+dayjs.extend(timezone);
+
+dayjs.tz.setDefault("Asia/Jakarta");
+
+export function checkDateValidity(d: Dayjs | Date) {
+  return dayjs(d).isValid();
+}
+
+export function formateDate1(date: Date | string | null): string {
+  if (!date) {
+    return "-";
+  } else {
+    return dayjs.utc(date).local().format("DD-MM-YYYY");
+  }
+}
