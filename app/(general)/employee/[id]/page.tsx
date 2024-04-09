c62b875c10e45394e33c090361e8ae2a8f2a869c
Yudistiraz <yudistiramastra@gmail.com>
2024-04-09 21:25:51 +0700

    feat : implemented api to get employee detail on employee id page

--- app/(general)/employee/[id]/page.tsx
+++ app/(general)/employee/[id]/page.tsx
@@ -1,18 +1,28 @@
 "use client";
 import EmployeeForm from "@/components/form/employee";
-import { DUMMY_EMPLOYEE } from "@/utils/constants";
-import { findDataById } from "@/utils/function";
-import { TEmployee } from "@/utils/models";
+import { getEmployeeById } from "@/services/admin/v1/employee";
 import { Typography } from "@mui/material";
+import { Fragment } from "react";
+import { useQuery } from "react-query";
 
 export default function EmployeeDetail({ params }: { params: { id: string } }) {
-  const data: TEmployee | undefined = findDataById(DUMMY_EMPLOYEE, params.id);
-  console.log(data);
-
+  const employeeDetailQuery = useQuery({
+    queryKey: ["employee", params.id],
+    queryFn: async () => {
+      const res = await getEmployeeById(params.id);
+      return res.data;
+    },
+  });
   return (
     <div className="tw-flex tw-flex-col tw-gap-6">
-      <Typography variant="h2">Employee : {params.id}</Typography>
-      <EmployeeForm isEdit data={data} />
+      {employeeDetailQuery.data && (
+        <Fragment>
+          <Typography variant="h2">
+            Employee : {employeeDetailQuery.data.name}
+          </Typography>
+          <EmployeeForm isEdit data={employeeDetailQuery?.data} />
+        </Fragment>
+      )}
     </div>
   );
 }
