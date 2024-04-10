20eb8b15aeed2ec16cd9123a204d9e5307bd77b5
Yudistiraz <yudistiramastra@gmail.com>
2024-04-10 21:10:29 +0700

    feat : created api for products

---
+++ services/admin/v1/product/index.ts
@@ -0,0 +1,42 @@
+import { api } from "@/services/config";
+interface ParamInterface {
+  [key: string]: number | string | unknown | undefined | null;
+}
+
+export const getAllProduct = async (params?: ParamInterface) => {
+  const fetcher = await api();
+
+  return fetcher.get("/products", {
+    params,
+  });
+};
+
+// export const addEmployee = async (data: any) => {
+//   const fetcher = await api();
+//   return fetcher.post("/user", data.data);
+// };
+
+// export const getEmployeeById = async (id: string) => {
+//   const fetcher = await api();
+//   return fetcher.get("/user/" + id);
+// };
+
+// export const updateEmployee = async (data: any) => {
+//   const fetcher = await api();
+//   return fetcher.patch("/user/" + data.id, data.data);
+// };
+
+// export const deleteEmployee = async (id: string) => {
+//   const fetcher = await api();
+//   return fetcher.delete("/user/" + id);
+// };
+
+// export const getAdminByID = async (id: string) => {
+//   const fetcher = await api();
+//   return fetcher.get(ADMIN_V1 + "/admins/" + id);
+// };
+
+// export const updateAdmin = async (data: any) => {
+//   const fetcher = await api();
+//   return fetcher.put(ADMIN_V1 + `/admins/${data.id}`, data.data);
+// };
