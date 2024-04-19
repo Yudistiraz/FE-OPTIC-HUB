bf2c9d80b193ebfe0b6e256f849d1dda3869c461
Yudistiraz <yudistiramastra@gmail.com>
2024-04-19 22:17:14 +0700

    feat : added isLoadingoption on product searchbar component

--- components/features/ProductSearchBar/index.tsx
+++ components/features/ProductSearchBar/index.tsx
@@ -1,10 +1,15 @@
 import { Search } from "@mui/icons-material";
-import React, { useState } from "react";
+import React, { Fragment, useState } from "react";
 
 import useDebounce from "@/hooks/debounce";
 
 import CustomTextField from "@/components/ui/TextField";
-import { Paper } from "@mui/material";
+import {
+  CircularProgress,
+  LinearProgress,
+  Paper,
+  Typography,
+} from "@mui/material";
 import ProductDropdownCard from "../ProductDropdownCard";
 import { TProduct } from "@/utils/models";
 import { isItemInArray } from "@/utils/function";
@@ -22,6 +27,7 @@ interface ProductSearchBarProps {
   productData?: TProduct[];
   selectedArray?: any[];
   onProductDropdownClick?: (data: TProduct) => void;
+  isLoading?: boolean;
 }
 
 export default function ProductSearchBar({
@@ -37,6 +43,7 @@ export default function ProductSearchBar({
   productData = [],
   selectedArray = [],
   onProductDropdownClick = (data: TProduct) => {},
+  isLoading = false,
 }: ProductSearchBarProps) {
   const [searchTerm, setSearchTerm] = useState("");
 
@@ -75,16 +82,24 @@ export default function ProductSearchBar({
       />
       {!hideDropdown && (
         <Paper className="tw-max-h-60 tw-overflow-y-scroll tw-absolute tw-w-full tw-bg-white tw-z-10 tw-p-2 tw-flex tw-flex-col tw-gap-2">
-          {productData.map((product) => (
-            <ProductDropdownCard
-              data={product}
-              disabled={isItemInArray(selectedArray, product)}
-              onClick={() => {
-                onProductDropdownClick(product);
-                resetInput();
-              }}
-            />
-          ))}
+          {isLoading ? (
+            <div className="tw-min-h-16 tw-flex tw-justify-center tw-items-center">
+              <CircularProgress size="2rem" />
+            </div>
+          ) : (
+            <Fragment>
+              {productData?.map((product) => (
+                <ProductDropdownCard
+                  data={product}
+                  disabled={isItemInArray(selectedArray, product)}
+                  onClick={() => {
+                    onProductDropdownClick(product);
+                    resetInput();
+                  }}
+                />
+              ))}
+            </Fragment>
+          )}
         </Paper>
       )}
     </div>
