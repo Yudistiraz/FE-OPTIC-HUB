80204482cbf9edd740b73350d99a7c15090df3c1
Yudistiraz <yudistiramastra@gmail.com>
2024-05-16 22:36:51 +0700

    feat : implemented barchart on general page

--- app/(general)/page.tsx
+++ app/(general)/page.tsx
@@ -1,7 +1,11 @@
 "use client";
 
-import { signOut } from "next-auth/react";
+import SalesBarChart from "@/components/features/SalesBarChart";
 
 export default function Home() {
-  return <div className="">test</div>;
+  return (
+    <div className="tw-flex tw-flex-col">
+      <SalesBarChart />
+    </div>
+  );
 }
