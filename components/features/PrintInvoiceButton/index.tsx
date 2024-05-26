37a848a2a8bf18cbe8aa4228cc7399fba9c27709
Yudistiraz <yudistiramastra@gmail.com>
2024-05-26 22:59:43 +0700

    feat : created print invoice button component

---
+++ components/features/PrintInvoiceButton/index.tsx
@@ -0,0 +1,28 @@
+import CustomButton from "@/components/ui/Button";
+import React, { Fragment, useRef } from "react";
+import ReactToPrint from "react-to-print";
+import InvoiceComponent from "../InvoiceComponent";
+import { TTransaction } from "@/utils/models";
+
+interface PrintInvoiceButtonProps {
+  data?: TTransaction;
+}
+
+export default function PrintInvoiceButton({ data }: PrintInvoiceButtonProps) {
+  const componentRef = useRef<HTMLDivElement>(null);
+  return (
+    <Fragment>
+      <ReactToPrint
+        trigger={() => (
+          <CustomButton type="button" className="tw-w-1/4">
+            Print Invoice
+          </CustomButton>
+        )}
+        content={() => componentRef.current}
+      />
+      <div className="tw-hidden">
+        <InvoiceComponent ref={componentRef} data={data} />
+      </div>
+    </Fragment>
+  );
+}
