b5fa60bb659f9722e0bbbd38db729f8c6d399024
Yudistiraz <yudistiramastra@gmail.com>
2024-05-27 15:43:21 +0700

    fix : fixed transaction skeleton loading

--- components/form/LoadingSkeletonForm/index.tsx
+++ components/form/LoadingSkeletonForm/index.tsx
@@ -48,55 +48,53 @@ export default function LoadingSkeletonForm({
   if (isTransactionForm) {
     return (
       <div className="tw-w-3/4 lg:tw-w-1/2 tw-flex tw-flex-col tw-gap-4">
-        <FormLayout>
+        <Skeleton variant="textField" />
+
+        <div className="tw-flex tw-gap-6">
           <Skeleton variant="textField" />
 
-          <div className="tw-flex tw-gap-6">
-            <Skeleton variant="textField" />
+          <Skeleton variant="textField" />
+        </div>
 
-            <Skeleton variant="textField" />
-          </div>
+        <div className="tw-w-full tw-flex tw-items-center tw-gap-4">
+          <Typography variant="subtitle1">Customer's Detail</Typography>
+          <Divider
+            flexItem
+            className="tw-my-4 tw-flex-grow"
+            sx={{
+              borderBottomWidth: 3,
+              bgcolor: "black",
+              borderColor: "black",
+            }}
+          />
+        </div>
 
-          <div className="tw-w-full tw-flex tw-items-center tw-gap-4">
-            <Typography variant="subtitle1">Customer's Detail</Typography>
-            <Divider
-              flexItem
-              className="tw-my-4 tw-flex-grow"
-              sx={{
-                borderBottomWidth: 3,
-                bgcolor: "black",
-                borderColor: "black",
-              }}
-            />
-          </div>
+        <Skeleton variant="textField" />
 
+        <div className="tw-flex tw-gap-6 tw-items-start">
           <Skeleton variant="textField" />
 
-          <div className="tw-flex tw-gap-6 tw-items-start">
-            <Skeleton variant="textField" />
-
-            <Skeleton variant="textField" />
-          </div>
-
-          <div className="tw-w-full tw-flex tw-items-center tw-gap-4">
-            <Typography variant="subtitle1">Transaction's Detail</Typography>
-            <Divider
-              flexItem
-              className="tw-my-4 tw-flex-grow"
-              sx={{
-                borderBottomWidth: 3,
-                bgcolor: "black",
-                borderColor: "black",
-              }}
-            />
-          </div>
-
           <Skeleton variant="textField" />
+        </div>
 
-          <Skeleton variant="textField" className="tw-h-36" />
+        <div className="tw-w-full tw-flex tw-items-center tw-gap-4">
+          <Typography variant="subtitle1">Transaction's Detail</Typography>
+          <Divider
+            flexItem
+            className="tw-my-4 tw-flex-grow"
+            sx={{
+              borderBottomWidth: 3,
+              bgcolor: "black",
+              borderColor: "black",
+            }}
+          />
+        </div>
 
-          <Skeleton variant="textField" />
-        </FormLayout>
+        <Skeleton variant="textField" />
+
+        <Skeleton variant="textField" className="tw-h-36" />
+
+        <Skeleton variant="textField" />
         <Skeleton variant="textField" className="tw-h-36" />
       </div>
     );
