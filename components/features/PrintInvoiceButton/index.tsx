a2077ae9ef5ccb2334fe0f64dc5e233dc15c397c
Yudistiraz <yudistiramastra@gmail.com>
2024-06-10 23:25:18 +0700

    feat : implemented language on printInvoice Button

--- components/features/PrintInvoiceButton/index.tsx
+++ components/features/PrintInvoiceButton/index.tsx
@@ -3,19 +3,21 @@ import React, { Fragment, useRef } from "react";
 import ReactToPrint from "react-to-print";
 import InvoiceComponent from "@/components/features/InvoiceComponent";
 import { TTransaction } from "@/utils/models";
+import { useLanguage } from "@/context/Language";
 
 interface PrintInvoiceButtonProps {
   data?: TTransaction;
 }
 
 export default function PrintInvoiceButton({ data }: PrintInvoiceButtonProps) {
+  const { translations } = useLanguage();
   const componentRef = useRef<HTMLDivElement>(null);
   return (
     <Fragment>
       <ReactToPrint
         trigger={() => (
           <CustomButton type="button" className="tw-w-1/4">
-            Print Invoice
+            {translations?.button?.printIvoice}
           </CustomButton>
         )}
         content={() => componentRef.current}
