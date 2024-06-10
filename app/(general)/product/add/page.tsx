1c651dbaf2540311eb512dfba13d34424ba57ddf
Yudistiraz <yudistiramastra@gmail.com>
2024-06-10 15:04:16 +0700

    feat : implemented language on product add and update page

--- app/(general)/product/add/page.tsx
+++ app/(general)/product/add/page.tsx
@@ -2,12 +2,16 @@
 
 import ProductForm from "@/components/form/ProductForm";
 import ComponentCard from "@/components/layout/ComponentCard";
+import { useLanguage } from "@/context/Language";
 import { Typography } from "@mui/material";
 
 export default function AddProduct() {
+  const { translations } = useLanguage();
   return (
     <div className="tw-flex tw-flex-col tw-gap-6 tw-w-full">
-      <Typography variant="h2">Add Product</Typography>
+      <Typography variant="h2">
+        {translations?.productPage?.createHeader}
+      </Typography>
       <ComponentCard>
         <ProductForm />
       </ComponentCard>
