b4ba752e80fe98dd7189087181236236dc851a6c
Yudistiraz <yudistiramastra@gmail.com>
2024-06-11 15:25:01 +0700

    feat : added error and helpertext on imageUpload component

--- components/ui/ImageUpload/index.tsx
+++ components/ui/ImageUpload/index.tsx
@@ -2,6 +2,7 @@ import { Add } from "@mui/icons-material";
 import React, { Fragment } from "react";
 import ImageLoader from "../ImageLoader";
 import ImageEditButtonProps from "@/components/features/ImageEditButton";
+import { Typography } from "@mui/material";
 
 interface ImageUploadProps {
   imageUrl?: string;
@@ -13,6 +14,8 @@ interface ImageUploadProps {
   onImageChange?: (data: File) => void | undefined;
   newImage?: File;
   onImageClear?: () => void;
+  error?: boolean;
+  helperText?: string;
 }
 
 const ImageUpload = ({
@@ -23,6 +26,8 @@ const ImageUpload = ({
   onImageChange = () => {},
   imageUrl,
   newImage,
+  error = false,
+  helperText = "",
   onImageClear = () => {},
 }: ImageUploadProps) => {
   const handleFileInputChange = async (
@@ -35,72 +40,71 @@ const ImageUpload = ({
   };
 
   return (
-    <div
-      className={`tw-w-40 tw-h-40 tw-rounded-md tw-overflow-hidden tw-relative ${
-        !imageUrl
-          ? "tw-bg-gray-400 tw-rounded-md tw-group tw-cursor-pointer tw-overflow-hidden"
-          : ""
-      }`}
-    >
-      {!imageUrl && (
-        <Fragment>
-          <label
-            htmlFor="avatar"
-            className="tw-cursor-pointer tw-w-full tw-h-full tw-flex tw-items-center tw-justify-center tw-p-2"
-          >
-            <div className="tw-w-full tw-h-full tw-border-dashed tw-border-4 tw-flex tw-items-center tw-justify-center tw-border-gray-500 tw-rounded-md group-hover:tw-border-gray-600 tw-duration-200">
-              <Add
-                fontSize="large"
-                className="tw-text-gray-500  group-hover:tw-text-gray-600 tw-duration-200"
-              />
-            </div>
-          </label>
-        </Fragment>
-      )}
-
-      {imageUrl && (
-        <Fragment>
-          <ImageLoader
-            alt="item-img"
-            className="tw-object-cover"
-            src={imageUrl}
-            priority
-            isFlat
-          />
-          {onEdit ? (
-            <Fragment>
-              {!newImage ? (
-                <label htmlFor="avatar">
-                  <ImageEditButtonProps
-                    onClick={onImageClear}
-                    type={!newImage && onEdit ? "edit" : "delete"}
-                  />
-                </label>
-              ) : (
-                <ImageEditButtonProps onClick={onImageClear} />
-              )}
-            </Fragment>
-          ) : (
-            <ImageEditButtonProps onClick={onImageClear} />
-          )}
-          {/* {onEdit && (
-            <ImageEditButtonProps
-              onClick={onImageClear}
-              type={!newImage && onEdit ? "edit" : "delete"}
+    <Fragment>
+      <div
+        className={`tw-w-40 tw-h-40 tw-rounded-md tw-overflow-hidden tw-relative ${
+          !imageUrl
+            ? "tw-bg-gray-400 tw-rounded-md tw-group tw-cursor-pointer tw-overflow-hidden"
+            : ""
+        } ${error ? "tw-outline tw-outline-[1px] tw-outline-danger-500" : ""}`}
+      >
+        {!imageUrl && (
+          <Fragment>
+            <label
+              htmlFor="avatar"
+              className="tw-cursor-pointer tw-w-full tw-h-full tw-flex tw-items-center tw-justify-center tw-p-2"
+            >
+              <div className="tw-w-full tw-h-full tw-border-dashed tw-border-4 tw-flex tw-items-center tw-justify-center tw-border-gray-500 tw-rounded-md group-hover:tw-border-gray-600 tw-duration-200">
+                <Add
+                  fontSize="large"
+                  className="tw-text-gray-500  group-hover:tw-text-gray-600 tw-duration-200"
+                />
+              </div>
+            </label>
+          </Fragment>
+        )}
+        {imageUrl && (
+          <Fragment>
+            <ImageLoader
+              alt="item-img"
+              className="tw-object-cover"
+              src={imageUrl}
+              priority
+              isFlat
             />
-          )} */}
-        </Fragment>
+            {onEdit ? (
+              <Fragment>
+                {!newImage ? (
+                  <label htmlFor="avatar">
+                    <ImageEditButtonProps
+                      onClick={onImageClear}
+                      type={!newImage && onEdit ? "edit" : "delete"}
+                    />
+                  </label>
+                ) : (
+                  <ImageEditButtonProps onClick={onImageClear} />
+                )}
+              </Fragment>
+            ) : (
+              <ImageEditButtonProps onClick={onImageClear} />
+            )}
+          </Fragment>
+        )}
+        <input
+          type="file"
+          id="avatar"
+          name="avatar"
+          accept="image/png, image/jpeg"
+          onChange={handleFileInputChange}
+          hidden
+        />
+      </div>
+      {error && (
+        <Typography variant="helperText" className="tw-text-danger tw-mx-0">
+          {helperText}
+        </Typography>
       )}
-
-      <input
-        type="file"
-        id="avatar"
-        name="avatar"
-        accept="image/png, image/jpeg"
-        onChange={handleFileInputChange}
-        hidden
-      />
-    </div>
+    </Fragment>
   );
 };
 
